2ee649ccc1c11d4879287f8ecd150266ad457e41
Xiaokang <147251139+xiaok80s@users.noreply.github.com>
2025-03-29 12:09:31 +0800

    Update fetch.sh

--- github/fetch.sh
+++ github/fetch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -sL https://api.github.com/meta | jq -r 'del(.["ssh_keys", "verifiable_password_authentication", "ssh_key_fingerprints", "domains", "actions_macos", "actions"]) | .[] | .[]' | grep -v ":" > github/ipv4.txt
+curl -sL https://api.github.com/meta | jq -r '{web,git,api} | .[] | .[]' | grep -v ":" > github/ipv4.txt
