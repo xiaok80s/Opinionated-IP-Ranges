a30ba1d413a3ccc9166d673f3925286b339246c8
Xiaokang <147251139+xiaok80s@users.noreply.github.com>
2025-07-22 20:26:21 +0800

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -5,5 +5,5 @@ find . -name fetch.sh | sort -h | awk '{print "Executing "$1"...";system("sh \""
 echo "Merging all IP ranges..."
 find . -name ipv4.txt | sort -h | xargs cat | cidrmerge > opinionated_ip_ranges.txt
 
-echo "Deleting ipv4.txt..."
-find . -name ipv4.txt -delete
+# echo "Deleting ipv4.txt..."
+# find . -name ipv4.txt -delete
